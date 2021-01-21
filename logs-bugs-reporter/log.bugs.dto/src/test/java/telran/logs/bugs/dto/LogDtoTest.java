812ba1315aa3782c94229c6e0dd5c6ec890f9cb4
Yeseniya <Yeseniya@10.0.0.10>
2021-01-21 16:17:44 +0200

    Added LogDto module with Unit Test

---
+++ logs-bugs-reporter/log.bugs.dto/src/test/java/telran/logs/bugs/dto/LogDtoTest.java
@@ -0,0 +1,44 @@
+package telran.logs.bugs.dto;
+
+import java.util.Date;
+
+import javax.validation.Valid;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+@WebMvcTest(LogDtoTest.TestController.class)
+@ContextConfiguration(classes=LogDtoTest.TestController.class)
+public class LogDtoTest {
+public static @RestController class TestController{
+	static LogDto logDtoExp=new LogDto(new Date(), LogType.NO_EXEPTION, "artifact", 0, "");
+	@PostMapping("/")
+	void testPost(@RequestBody @Valid LogDto logDto){
+		assertEquals(logDtoExp,logDto);
+	}
+}
+ObjectMapper mapper =new ObjectMapper();
+@Autowired
+MockMvc mock;
+@Test
+void testPostRun() throws Exception {
+	assertEquals(200,mock.perform(post("/").contentType(MediaType.APPLICATION_JSON)
+			.content(mapper.writeValueAsString(TestController.logDtoExp))).andReturn().getResponse().getStatus());
+}
+}
